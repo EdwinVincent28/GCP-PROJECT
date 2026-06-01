5b91edc6fa7ab718a152d7c4e64cd139ec49c819
EdwinVincent28 <edwinvincent.evt@gmail.com>
2026-06-02 01:28:34 +0200

    feat: User model added and JWT setup configured

--- app/backend/core/config.py
+++ app/backend/core/config.py
@@ -2,6 +2,9 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     MONGO_URI: str
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     
     class Config:
         env_file = ".env"
