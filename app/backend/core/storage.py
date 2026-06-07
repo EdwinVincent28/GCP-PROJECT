7a571713a69a1045a5d6eff232d55a041be1adc1
EdwinVincent28 <edwinvincent.evt@gmail.com>
2026-06-07 23:32:36 +0200

    minor changes

--- app/backend/core/storage.py
+++ app/backend/core/storage.py
@@ -11,6 +11,6 @@ def upload_image_to_gcs(file_obj, filename: str, content_type: str) -> str:
     
     blob = bucket.blob(unique_filename)
     blob.upload_from_file(file_obj, content_type=content_type)
-    blob.make_public()
+    # blob.make_public()
     
     return blob.public_url
