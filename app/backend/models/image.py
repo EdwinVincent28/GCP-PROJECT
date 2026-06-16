8eec67907babd018f6c7aeb0b8ca733c5a7df3f3
EdwinVincent28 <edwinvincent.evt@gmail.com>
2026-06-16 15:55:05 +0200

    feat: Detecting objects using Yolo after upload

--- app/backend/models/image.py
+++ app/backend/models/image.py
@@ -13,3 +13,4 @@ class ImageResponse(BaseModel):
     filename: str
     image_url: str
     status: str
+    detected_objects: List[str]
--- app/backend/routes/image_routes.py
+++ app/backend/routes/image_routes.py
@@ -4,7 +4,14 @@ from core.storage import upload_image_to_gcs
 from routes.auth_routes import get_current_user
 from models.image import ImageMetadata, ImageResponse
 
+from ultralytics import YOLO
+import cv2
+import numpy as np
+
 router = APIRouter()
+
+print("Initializing YOLOv8 object detection model...")
+model = YOLO("yolov8n.pt")
 
 @router.post("/upload", response_model=ImageResponse)
 async def upload_image(
@@ -17,6 +24,22 @@ async def upload_image(
         raise HTTPException(status_code=400, detail="File provided is not an image.")
 
     try:
+        contents = await file.read()
+        
+        await file.seek(0)
+
+        nparr = np.frombuffer(contents, np.uint8)
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        
+        detected_items = []
+        if img is not None:
+            results = model(img, verbose=False)
+            for r in results:
+                for c in r.boxes.cls:
+                    detected_items.append(model.names[int(c)])
+
+            detected_items = list(set(detected_items))
+
         gcs_url = upload_image_to_gcs(file.file, file.filename, file.content_type)
         
         image_data = ImageMetadata(
@@ -28,6 +51,7 @@ async def upload_image(
         collection = db["images"]
         doc_to_insert = image_data.model_dump()
         doc_to_insert["owner_email"] = current_user["email"]
+        doc_to_insert["detected_objects"] = detected_items  
         
         result = await collection.insert_one(doc_to_insert)
         
@@ -35,7 +59,8 @@ async def upload_image(
             id=str(result.inserted_id),
             filename=file.filename,
             image_url=gcs_url,
-            status="Successfully uploaded to GCP and metadata saved."
+            status="Successfully uploaded to GCP and metadata saved.",
+            detected_objects=detected_items  
         )
         
     except Exception as e:
